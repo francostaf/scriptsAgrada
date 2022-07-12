ece224e29244df3a6a98c00ca59ccce0e8897c27
Franciele Costa da Fonseca <83457948+francostaf@users.noreply.github.com>
2022-07-12 17:28:23 -0300

    Update mouserOver.js

--- functions/mouserOver.js
+++ functions/mouserOver.js
@@ -1,1 +1,7 @@
+$( ".wd-product-line" ).mouseover(function() {
+  $(this).children('.item-description').children('.bottom').css( "display","block" );
+});
 
+$( ".wd-product-line" ).mouseout(function() {
+  $(this).children('.item-description').children('.bottom').css( "display","none" );
+});
