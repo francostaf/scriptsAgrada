fbac604e0e515906ad007c79b66e54745db6ab96
Franciele Costa da Fonseca <83457948+francostaf@users.noreply.github.com>
2022-09-23 09:16:57 -0300

    Update specialCharacters.js

--- functions/specialCharacters.js
+++ functions/specialCharacters.js
@@ -1,8 +1,8 @@
 $(function() {
-	var regex = new RegExp('[^.0-9@a-zA-Zàèìòùáéíóúâêîôûãõ\b-]', 'g');
-	$('input#novoCadastroSobrenome, input#novoCadastroNome, input#novoCadastroEmail').bind('input', function(){
-		$(this).val($(this).val().replace(regex, ''));
+	//bloqueia caracteres especiais cadastro nome e sobrenome
+	var regexNome = new RegExp("[^'ç~@0-9 a-zA-Zàèìòùáéíóúâêîôûãõ\b]", "g");
+	$('input#AddOrSetCustomer-Name,input#AddOrSetCustomer-Surname').bind('input', function(){
+		$(this).val($(this).val().replace(regexNome, ''));
 	});
 });
-
 https://regexr.com/
